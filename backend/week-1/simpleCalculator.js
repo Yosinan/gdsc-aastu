2f86cb5adfdc7e960d52308a61364d85ae5cc2f9
Yosinan <ymymym4634@gmail.com>
2023-03-16 22:25:38 +0300

    week-1

---
+++ backend/week-1/simpleCalculator.js
@@ -0,0 +1,9 @@
+#!/bin/node
+
+// a simple calculator
+
+console.log('Simple Calculator');
+
+const x = prompt('Enter the 1st number');
+const oprator = prompt('Enter the operator (+, -, *, /, %)');
+const y = prompt('Enter the 2nd number');
