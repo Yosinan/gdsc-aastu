5b96a14340894575ab7acc4f3ff9670b62a3fe79
Yosinan <ymymym4634@gmail.com>
2023-03-16 22:40:57 +0300

    week-1

--- backend/week-1/simpleCalculator.js
+++ backend/week-1/simpleCalculator.js
@@ -7,3 +7,23 @@ console.log('Simple Calculator');
 const x = prompt('Enter the 1st number');
 const oprator = prompt('Enter the operator (+, -, *, /, %)');
 const y = prompt('Enter the 2nd number');
+
+
+switch(operator) {
+	case '+':
+		console.log('Sum = '+ (x + y));
+		break;
+	case '-':
+		console.log('Difference = ' + (x - y));
+		break;
+	case '*':
+		console.log('Product = '+  (x * y));
+		break;
+	case '/':
+		console.log('Quotient = ' + (x / y));
+	case '%':
+		console.log('Modulus = ' + (x % y));
+		break;
+	default:
+		console.log('Invalid input');
+}
