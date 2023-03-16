8e3b6a48b380e22093c5188dab22661a91a3a023
Yosinan <ymymym4634@gmail.com>
2023-03-16 23:22:09 +0300

    week-1

--- backend/week-1/simpleCalculator.js
+++ backend/week-1/simpleCalculator.js
@@ -1,30 +1,34 @@
 #!/bin/node
-
 // a simple calculator
 
 console.log('Simple Calculator');
 
-const x = parseFloat(prompt('Enter the 1st number'));
-const oprator = prompt('Enter the operator (+, -, *, /, %)');
-const y = parseFloat(prompt('Enter the 2nd number'));
+const x = parseFloat(prompt('Enter the 1st number '));
+const operator = prompt('Enter the operator (+, -, *, /, %) ');
+const y = parseFloat(prompt('Enter the 2nd number '));
 let res;
+
 
 switch (operator) {
   case '+':
     res = x + y;
+    
     break;
   case '-':
     res = x - y;
+    
     break;
   case '*':
     res = x * y;
+   
     break;
   case '/':
     res = x / y;
+    break;
   case '%':
     res = x % y;
     break;
   default:
     console.log('Invalid input');
 }
-console.log('${x} ${operator} ${y} = ${res}');
+console.log(x, operator, y, '=' , res);
