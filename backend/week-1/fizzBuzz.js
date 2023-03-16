20676bdaa0c26dbb4e9acd220b813831faca7a32
Yosinan <ymymym4634@gmail.com>
2023-03-16 22:29:05 +0300

    week-1

--- backend/week-1/fizzBuzz.js
+++ backend/week-1/fizzBuzz.js
@@ -1,6 +1,6 @@
 #!/bin/node
 
-// a script that will print nos from 1 to 50 and for multiples of 3 it will print "Fizz", for the multiples of 5 it will print "Buzz",  and for the multiples of both it will print "FIzzBuzz"
+//  a script that will print nos from 1 to 50 and for multiples of 3 it will print "Fizz", for the multiples of 5 it will print "Buzz",  and for the multiples of both it will print "FIzzBuzz"
 
 for (let i = 1; i <= 50; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
