5465eae388d3670fb7d163d1bc0b3eeb4ddc0676
Yosinan <ymymym4634@gmail.com>
2023-03-22 15:27:13 +0300

    update

---
+++ backend/new.js
@@ -0,0 +1,5 @@
+function doubl (arr) {
+arr.map(arr, (x) => x *2 )
+}
+module.exports = doubl
+
---
+++ backend/sth.js
@@ -0,0 +1,7 @@
+const im = require('./new')
+//const im = require('lodash')
+
+
+const arr = [1, 2, 3, 4]
+
+console.log(im.doubl(arr, x => x * 2)) 
